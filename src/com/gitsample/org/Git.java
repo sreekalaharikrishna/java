154fee2ef5ef3980d913059f9e54514503460d91
unknown <sreekalahari89@gmail.com>
2019-12-04 22:59:55 +0530

    javacode

---
+++ src/com/gitsample/org/Git.java
@@ -0,0 +1,19 @@
+package com.gitsample.org;
+
+public class Git {
+	private void callingM1() {
+		System.out.println("firstM");
+
+	}
+	private void callingM2() {
+		System.out.println("firstM21");
+
+	}
+	public static void main(String[] args) {
+		Git g = new Git();
+		g.callingM1();
+		g.callingM2();
+
+	}
+
+}
